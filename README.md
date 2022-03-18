d56057cd800cec1ba52fe89be7e7fdb7a450bb1b
Caleb Petit <caleb.petit@gmail.com>
2022-03-18 14:09:30 -0700

    Added initial Resume content

---
+++ README.md
@@ -0,0 +1,2 @@
+This page is a webpage of my resume and associated work
+With links to apps i created
---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+import addRecipeView from "./views/addRecipeView.js";
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) Loading recipe
+    await model.loadRecipe(id);
+
+    // 3) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err);
+  }
+};
